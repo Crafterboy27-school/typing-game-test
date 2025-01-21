9e457bef4c89fcae418bec86099993c6ed2d7c1d
Crafterboy27-school <95499245+Crafterboy27-school@users.noreply.github.com>
2025-01-21 19:51:21 +0000

    generate random words

--- web/js/words.js
+++ web/js/words.js
@@ -1,1 +1,21 @@
-export const words = await ((await fetch("/data/words.txt")).text())
+export const words = ( (await ((await fetch("/data/words.txt")).text())) ).split("\n")
+
+export function GetRandomWords(count = 1){
+    let ret = []
+    for(let i = 0;i<count;i++){
+        ret.push(words[Math.floor(Math.random()*words.length)])
+    }
+
+    return ret
+}
+
+export function ConvertStringToSpans(str){
+    return str.split("").map(char=>{
+        let el = document.createElement("span")
+        ;el.textContent = char;
+        return el
+    })
+}
+
+let spans = ConvertStringToSpans(GetRandomWords(10).join(" "))
+document.querySelector("#letters").append(...spans)
